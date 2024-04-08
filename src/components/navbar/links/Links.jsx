b1762734d8e39b0cc2139aee22ca093162595054
Venkatesh <venkeyp6@gmail.com>
2024-04-09 01:05:36 +0530

    Active class given to Navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import styles from "./links.module.css"
+import NavLink from "../links/navLink/navLink.jsx"
 
 const Links = () => {
     const links = [
@@ -19,11 +20,27 @@ const Links = () => {
             path: "/blog"
         }
     ]
+    
+    
+    //TEMPORARY
+
+    const session = true
+    const isAdmin = true
+
     return (
-        <div>
+        <div className={styles.links}>
             {links.map(link => 
-                <Link href={link.path} key={link.title}>{link.title}</Link>
-            )}
+                <NavLink item={link} key={link.title} />
+            )}{
+                session ? (
+                    <>
+                        {isAdmin &&  <NavLink item={{title: "Admin", path:"/admin"}} />}
+                        <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLink item={{title: "Login", path:"/login"}} />
+                )
+            }
         </div>
     )
 }

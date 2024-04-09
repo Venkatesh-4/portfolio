816f3e512191c7763fea2e80a6999f37357423ac
Venkatesh <venkeyp6@gmail.com>
2024-04-10 01:21:35 +0530

    Commit before attempting to git clone a single file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
+import styles from "./home.module.css"
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return <div className={styles.container}>Homepage
+  <div className={styles.textContainer}>
+    <h1>Head Title</h1>
+    <p>This consists of a paragraph right inder the main title of this page</p>
+    <div className={styles.buttons}>
+      <button className={styles.button}>Learn More</button>
+      <button className={styles.button}>Contact</button>
+    </div>
+    <div className={styles.brands}>
+      <Image />
+    </div>
+  </div>
+  <div className={styles.imageContainer}></div>
+  </div>
 };
 
 export default Home;

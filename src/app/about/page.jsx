efaec259f50a0333261ec88720fd30bf29fb281f
Venkatesh <venkeyp6@gmail.com>
2024-04-10 02:51:12 +0530

    About page design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,37 @@
+import styles from "./about.module.css"
+import Image from "next/image"
+
 const AboutPage = () => {
     return(
-        <div>AboutPage</div>
+        <div className={styles.container}>
+        <div className={styles.textContainer}>
+            <h2 className={styles.subtitle}>About Agency</h2>
+            <h1 className={styles.title}>We are harder stronger fast and slower at the same time yo</h1>
+            <p className={styles.desc}>This is a big ass para and has quite few lines yo yo yo This is a big ass para and has quite few lines yo yo yo This is a big ass para and has quite few lines yo yo yo This is a big ass para and has quite few lines yo yo yo </p>
+             <div className={styles.boxes}>
+                <div className={styles.box}>
+                    <h1>10K+</h1>
+                    <p>Years of experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h1>10K+</h1>
+                    <p>Years of experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h1>10K+</h1>
+                    <p>Years of experience</p>
+                </div>
+             </div>
+        </div>
+        <div className={styles.imgContainer}>
+            <Image 
+                src="/about.png"
+                alt="About Image"
+                fill
+                className={styles.img}
+            />
+        </div>
+        </div>
     )
 }
 

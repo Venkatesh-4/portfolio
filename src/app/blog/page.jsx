647848c741cb5b027729e60ad27a3084f7af2fbf
Venkatesh <venkeyp6@gmail.com>
2024-04-10 19:16:01 +0530

    blogpage design done

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,12 +1,13 @@
 import styles from "./blog.module.css"
+import PostCard from "../../components/postCard/postCard.jsx"
 
 const BlogPage = () => {
     return(
         <div className={styles.container}>
-            <PostCard />
-            <PostCard />
-            <PostCard />
-            <PostCard />
+            <div className={styles.post}><PostCard /></div>
+            <div className={styles.post}><PostCard /></div>
+            <div className={styles.post}><PostCard /></div>
+            <div className={styles.post}><PostCard /></div>
         </div>
     )
 }
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -7,14 +7,14 @@ const postCard = () => {
         <div className={styles.container}>
             <div className={styles.top}>
                 <div className={styles.imgContainer}>
-                    <Image src="/post.png" alt="" fill className={styles.img} />
+                    <Image src="https://images.pexels.com/photos/20877176/pexels-photo-20877176/free-photo-of-general-archive-of-the-indies-in-seville.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
                 </div>
                 <span className={styles.date}>23.12.1999</span>
             </div>
             <div className={styles.bottom}>
                 <h1 className={styles.title}>Title</h1>
-                <p className={styles.desc}>Desc</p>
-                <Link href="/blog/post">READ MORE</Link>
+                <p className={styles.desc}>long text to check how it is in the screen long text to check how it is in the screen long text to check how it is in the screen long text to check how it is in the screen long text to check how it is in the screen</p>
+                <Link className={styles.link} href="/blog/post">READ MORE</Link>
             </div>
         </div>
     )

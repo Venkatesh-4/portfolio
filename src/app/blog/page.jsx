8eff0e520d761b1eb20aa170878bb91c90ef9c73
Venkatesh <46065241+Venkatesh-4@users.noreply.github.com>
2024-06-23 20:09:51 +0530

    Revert "Fetching blog posts via api endpoint"
    This reverts commit d760e7045aedcc5045687ce52e0e067f1d8b6704.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,26 +2,13 @@ import styles from "./blog.module.css";
 import PostCard from "../../components/postCard/postCard.jsx";
 import { getPosts } from "@/lib/data";
 
-//API data fetch
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/blog", {
-    next: { revalidate: 3600 },
-  });
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-  return res.json();
-};
-
 export const metadata = {
   title: "Blog Page",
   description: "Blog Description",
 };
 
 const BlogPage = async () => {
-  // const posts = await getPosts();
-  const posts = await getData();
-
+  const posts = await getPosts();
   return (
     <div className={styles.container}>
       {posts.map((post) => (

f7952065bc24e086affba18009caa2b651381d75
Venkatesh <46065241+Venkatesh-4@users.noreply.github.com>
2024-06-23 13:11:46 +0530

    lot of stuff changed
    Mainly the database part and added params to blog page and slug page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,37 +1,49 @@
-import styles from "./singlePost.module.css"
-import Image from "next/image"
-import PostUser from "@/components/postUser/postUser"
-import { Suspense } from "react"
-import { getPost } from "@/lib/data"
+import styles from "./singlePost.module.css";
+import Image from "next/image";
+import PostUser from "@/components/postUser/postUser";
+import { Suspense } from "react";
+import { getPost } from "@/lib/data";
 
-const SinglePostPage = async ({params}) => {
-    const { slug } = params;
-    const post = await getPost(slug);
-    // console.log(post)
-    return(
-        <div className={styles.container}>
-            <div className={styles.imgContainer}>
-                <Image src="https://images.pexels.com/photos/20877176/pexels-photo-20877176/free-photo-of-general-archive-of-the-indies-in-seville.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.img} fill />
-            </div>
-            <div className={styles.textContainer}>
-                <h1 className={styles.title}>{post?.title}</h1>
-                <div className={styles.detail}>
-                    <Image src="https://images.pexels.com/photos/20877176/pexels-photo-20877176/free-photo-of-general-archive-of-the-indies-in-seville.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.avatar} width={50} height={50} />
-                    {post && (
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <PostUser userId={post.userId}/>
-                        </Suspense>
-                    )}
-                        
-                    <div className={styles.detailText}>
-                        <span className={styles.detailTitle}>Published</span>
-                        <span className={styles.detailValue}>23.12.1999</span>
-                    </div>
-                </div>
-                <div className={styles.content}>{post?.body}</div>
-            </div>
+const SinglePostPage = async ({ params }) => {
+  console.log(params);
+  const { slug } = params;
+  const post = await getPost(slug);
+  // console.log(post)
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/20877176/pexels-photo-20877176/free-photo-of-general-archive-of-the-indies-in-seville.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className={styles.img}
+          fill
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post?.title}</h1>
+        <div className={styles.detail}>
+          <Image
+            src="https://images.pexels.com/photos/20877176/pexels-photo-20877176/free-photo-of-general-archive-of-the-indies-in-seville.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.avatar}
+            width={50}
+            height={50}
+          />
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
+
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>23.12.1999</span>
+          </div>
         </div>
-    )
-}
+        <div className={styles.content}>{post?.body}</div>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePostPage
+export default SinglePostPage;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,17 +1,18 @@
-import styles from "./blog.module.css"
-import PostCard from "../../components/postCard/postCard.jsx"
-import { getPosts } from "@/lib/data"
-const BlogPage = async () => {
-    const posts = await getPosts();
-    return(
-        <div className={styles.container}>
-            {posts.map((post) => (
-            <div className={styles.post} key={post.id}>
-                <PostCard post={post}/>
-            </div>
-            ))}
+import styles from "./blog.module.css";
+import PostCard from "../../components/postCard/postCard.jsx";
+import { getPosts } from "@/lib/data";
+const BlogPage = async ({ params, searchParams }) => {
+  console.log(searchParams);
+  const posts = await getPosts();
+  return (
+    <div className={styles.container}>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default BlogPage
+export default BlogPage;

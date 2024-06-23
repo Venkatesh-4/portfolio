b5db47a42052637d65b15db3970e31c379ae8eda
Venkatesh <46065241+Venkatesh-4@users.noreply.github.com>
2024-06-23 16:30:19 +0530

    User name and date posted along with image modified
    Moved avatar handling from single post page to postUser component. Included dynamic date.
    Added necessary css for detail text and value.
    Added nessesary avatar css.

--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import styles from "./postUser.module.css";
 import { getUser } from "@/lib/data";
 const PostUser = async ({ userId }) => {
   const user = await getUser(userId);
   return (
-    <div className={styles.Container}>
-      <span className={styles.title}>Author</span>
-      <span className={styles.username}>{user.username}</span>
+    <div className={styles.container}>
+      <Image
+        src={user.img ? user.img : "/noavatar.png"}
+        alt=""
+        className={styles.avatar}
+        width={50}
+        height={50}
+      />
+      <div className={styles.texts}>
+        <span className={styles.title}>Author</span>
+        <span className={styles.username}>{user.username}</span>
+      </div>
     </div>
   );
 };

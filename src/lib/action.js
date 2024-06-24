401dfef2bd8aaebccbe92ac52fb893558766d3ba
Venkatesh <46065241+Venkatesh-4@users.noreply.github.com>
2024-06-24 19:09:48 +0530

    Password hashing
    Installed bcrypt.
    Used a bcrypt function to hash the password during registration. In the DB is hashed password is saved.

--- src/lib/action.js
+++ src/lib/action.js
@@ -3,6 +3,7 @@ import { revalidatePath } from "next/cache";
 import { Post, User } from "./models";
 import { connectToDb } from "./utils";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcrypt";
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -65,10 +66,13 @@ export const register = async (formData) => {
       // console.log(message);
       return message;
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       img,
     });
     await newUser.save();

022ae0987d736cb99cf1a746e36a30a8f3b22ec3
Venkatesh <46065241+Venkatesh-4@users.noreply.github.com>
2024-06-24 15:28:11 +0530

    Saving user details in the database when logged in through GitHub
    callback function written check the data base if the user logged on through GitHub exists, if not add the user to the users table.
    altered the users table model to change the password field from being mandatory to facilitate the addition of user data when logged on through GitHub

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { connectToDb } from "./utils";
+import { User } from "./models";
 
 export const {
   handlers: { GET, POST },
@@ -13,4 +15,26 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider === "github") {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile.email });
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              image: profile.avatar_url,
+            });
+            await newUser.save();
+          }
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 });
